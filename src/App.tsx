444788ea3f70d00282c983dd79a2971dbecbdbb5
mohammad <mohammadnv79@gmail.com>
2022-08-09 16:51:04 +0430

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="">
+        <img src={logo} className="" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
